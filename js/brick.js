ca66a5ef1b1b5218405820d33c91dd89c3bd16fc
damdat97 <dam.dat97@gmail.com>
2022-03-29 20:30:17 +0700

    add bai

---
+++ js/brick.js
@@ -0,0 +1,59 @@
+arr = [];
+
+function Brick() {
+    this.offSetX = OFF_SET_X;
+    this.offSetY = OFF_SET_Y;
+    this.marginBetween = MARGIN_BETWEEN;
+    this.width = WIDTH_BRICKS;
+    this.height = HEIGHT_BRICKS;
+    this.totalRows = TOTAL_ROWS;
+    this.totalCols = TOTAL_COLS;
+    
+    this.xyCoordinate = function () {
+        for (let i = 0; i< this.totalRows; i++) 
+        {
+            for (let j=0; j<this.totalCols; j++)
+            {
+                arr.push(
+                    {
+                        oX: this.offSetX+j*(this.width + this.marginBetween),
+                        oY: this.offSetY+i*(this.height + this.marginBetween),
+                        statusBricks: true
+                    });
+            }
+        }
+    };
+    
+    this.drawBricks = function (bricks)
+    {
+        arr.forEach(function (element)
+        {
+            if (element.statusBricks)
+            {
+                ctx.beginPath();
+                ctx.rect(element.oX, element.oY, bricks.width, bricks.height);
+                ctx.fill();
+                ctx.closePath();
+            }
+        })
+    };
+    
+    this.ballImpactBricks = function (ball, bricks)
+    {
+        arr.forEach(function (element)
+        {
+            if (element.statusBricks)
+            {
+                if (ball.x >= element.oX &&    //Ball va cham goc tren ben phai
+                    ball.x <= element.oX + bricks.width &&     //Ball va cham goc tren ben trai
+                    ball.y + ball.radius >= element.oY &&      //Ball va cham goc duoi ben phai
+                    ball.y - ball.radius <= element.oY + bricks.height)    //ball va cham goc duoi ben trai
+                {
+                    ball.dy = -ball.dy;      //Ball cham vao paddle di len la -dy => khi cham bricks ball se thanh -(-dy)
+                    element.statusBricks = false;
+                    scores++;
+                }
+            }
+        });
+    };
+}
---
+++ js/paddle.js
@@ -0,0 +1,57 @@
+function Paddle()
+{
+        this.width = WIDTH_PADDLE;
+        this.height = HEIGHT_PADDLE;
+        this.x = X_PADDLE;
+        this.y = Y_PADDLE;
+        this.speed = SPEED;
+        this.color = getRandomColor();
+        this.isRight = false;
+        this.isLeft = false;
+
+    this.drawPaddle = function ()
+    {
+        let x = this.x;
+        let y = this.y;
+        let width = this.width;
+        let height = this.height
+        ctx.beginPath();
+        ctx.rect(x, y, width, height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    };
+
+    this.moveLeft = function ()
+    {
+        if (this.isLeft)
+        {
+            this.x = this.x  - this.speed;
+        }
+    };
+
+    this.moveRight = function () {
+        if (this.isRight)
+        {
+            this.x = this.x + this.speed;
+        }
+    };
+
+    this.movePaddle = function () {
+        this.moveLeft();
+        this.moveRight();
+        if (this.x < 0)
+        {
+            // Neu toa do x < 0 thi set x = 0 (Truong hop canh ben trai)
+            this.x = 0;
+        }
+        else
+        {
+            if (this.x > canvas.width - this.width)
+            {
+                //Neu toa do x > gioi han ben phai thi set x = canvas.width - this.width = 500 - width paddle nhap vao (Truong hop canh ben phai)
+                this.x = canvas.width - this.width;
+            }
+        }
+    };
+}
